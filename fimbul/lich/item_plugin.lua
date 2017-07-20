bbdc5475fdee8c9db896b5baae9ee80617e19c50
Florian Stinglmayr <florian@n0la.org>
2017-07-20 18:20:08 +0200

    split it up into separate plugins

--- fimbul/lich/item_plugin.lua
+++ fimbul/lich/item_plugin.lua
@@ -2,13 +2,13 @@
 --
 
 local plugin = require('lich.plugin')
-local demi_lich = plugin:new()
+local item_plugin = plugin:new()
 
 local repository = require('fimbul.repository')
 local log = require('lich.log')
 local base = _G
 
-function demi_lich:new(config)
+function item_plugin:new(config)
    local neu = plugin:new(config)
 
    setmetatable(neu, self)
@@ -16,27 +16,27 @@ function demi_lich:new(config)
 
    self.config = config or {}
 
-   assert(self.config.game, 'demi_lich: no game specified')
+   assert(self.config.game, 'item_plugin: no game specified')
 
    return neu
 end
 
-function demi_lich:name()
-   return 'fimbul.demi_lich'
+function item_plugin:name()
+   return 'fimbul.item_plugin'
 end
 
-function demi_lich:onconnect(server)
+function item_plugin:onconnect(server)
    if not self.repository then
       ok, r = pcall(repository.new, repository, self.config.game)
       if not ok then
-         log.error('demi_lich: error loading repo: ' .. self.repository)
+         log.error('item_plugin: error loading repo: ' .. self.repository)
       else
          self.repository = r
       end
    end
 end
 
-function demi_lich:on_item(server, user, channel, cmd)
+function item_plugin:on_item(server, user, channel, cmd)
    local i = table.concat(cmd.args, ' ')
    local msg
 
@@ -44,20 +44,20 @@ function demi_lich:on_item(server, user, channel, cmd)
    ok, item = pcall(self.repository.parse_item, self.repository, i)
    if not ok or item == nil then
       msg = 'Not a valid item: ' .. i
-      log.error('demi_lich: not a valid item: ' .. i)
+      log.error('item_plugin: not a valid item: ' .. i)
    else
       msg = item:string(true)
-      log.info('demi_lich: item: ' .. item:string(true))
+      log.info('item_plugin: item: ' .. item:string(true))
    end
 
    server:send_message(user, channel, msg)
 end
 
-function demi_lich:oncommand(server, user, channel, cmd)
+function item_plugin:oncommand(server, user, channel, cmd)
    -- see what sort of command it was
    if cmd.command == 'item' then
       self:on_item(server, user, channel, cmd)
    end
 end
 
-return demi_lich
+return item_plugin
